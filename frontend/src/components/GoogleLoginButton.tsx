110ce98301470c44cebabf0ad8dbcd16b460a42a
Christoph Wiese <32820890+WieseChristoph@users.noreply.github.com>
2025-03-06 23:42:25 +0100

    Fix formatting

--- frontend/src/components/GoogleLoginButton.tsx
+++ frontend/src/components/GoogleLoginButton.tsx
@@ -7,9 +7,9 @@ const GoogleLoginButton: React.FC = () => {
 		>
 			{"Login with "}
 			{"Google".split("").map((letter, index) => (
-                <span
-                    // biome-ignore lint/suspicious/noArrayIndexKey:
-					key={index} 
+				<span
+					// biome-ignore lint/suspicious/noArrayIndexKey:
+					key={index}
 					className={`font-bold ${index === 0 || index === 3 ? "text-[#4285F4]" : index === 1 || index === 5 ? "text-[#EA4335]" : index === 2 ? "text-[#FBBC04]" : "text-[#34A853]"}`}
 				>
 					{letter}
